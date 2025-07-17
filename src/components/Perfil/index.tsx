f646f6cbb65dc71d1b2065f2c121e2f5e6aaac44
Flavio Irala <flavioiralap@gmail.com>
2025-07-17 14:32:41 -0400

    Primeiro commit

---
+++ src/components/Perfil/index.tsx
@@ -0,0 +1,11 @@
+import { ContainerPerfil } from "./styles";
+
+const Perfil = () => {
+    return (
+        <ContainerPerfil>
+            <h1>Nome Perfil</h1>
+        </ContainerPerfil>
+    );
+};
+
+export default Perfil;
---
+++ src/components/Perfil/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components"
+
+export const ContainerPerfil = styled.div`
+    display: block;
+`
