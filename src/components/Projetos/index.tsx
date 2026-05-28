e9f9cb5591b6011b02ab2b0d6875d9d864a87ff0
Flávio Irala <flavioiralap@gmail.com>
2026-05-27 20:06:24 -0400

    Salvamento de Backup

--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -3,9 +3,22 @@ import * as S from "./styles"
 const Projetos = () => {
     return (
      <S.ContainerProjeto>
-        <h1>
+        <S.TitleProj>
             Projetos
-        </h1>
+        </S.TitleProj>
+        <S.ProjList>
+            <S.ProjCenter className="row mr-0 ">
+                <S.ProjItem className="col-lg-2 col-md-4 col-sm-6">
+                    <h2>Item</h2>
+                </S.ProjItem>
+                <S.ProjItem className="col-lg-2 col-md-4 col-sm-6">
+                    <h2>Item</h2>
+                </S.ProjItem>
+                <S.ProjItem className="col-lg-2 col-md-4 col-sm-6">
+                    <h2>Item</h2>
+                </S.ProjItem>
+            </S.ProjCenter>
+        </S.ProjList>
      </S.ContainerProjeto>
     )
 }
--- src/components/Projetos/styles.ts
+++ src/components/Projetos/styles.ts
@@ -1,6 +1,40 @@
 import styled from "styled-components"
 
-export const ContainerProjeto = styled.div `
-    width: 500px;
+export const ContainerProjeto = styled.div`
+    width: 100%;
     background-color: #444444ff;
+`
+
+export const TitleProj = styled.h1`
+    width: 100%;
+    text-align: center;
+    font-size: clamp(40px, 6vw, 60px);
+    color: #38bdae;
+    font-weight: bold;
+` 
+export const ProjList = styled.div`
+    background-color: #444444ff;
+    margin: 0 auto;
+    display: grid;
+    list-style: none;
+` 
+
+export const ProjCenter = styled.div`
+    display: flex;
+    justify-content: space-around;
+`
+
+export const ProjItem = styled.div`
+    background-color: black;
+    cursor: pointer;
+    margin: 10px;
+    border-radius: 10px;
+    margin-right: 0;
+
+    h2 {
+        margin-top: 10px;
+        text-align: center;
+        font-size: clamp(20px, 3vw, 30px);
+    }
+
 `
